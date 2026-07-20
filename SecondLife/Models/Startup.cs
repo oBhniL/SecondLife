bb836d77b21f2916d7bad4ebb8fe02284f350d96
oBhniL <hoanglebaolinh9.2@gmail.com>
2026-07-20 13:34:31 +0700

    Tạo mới Startup

---
+++ SecondLife/Models/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using SecondLife.Models;
+
+[assembly: OwinStartup(typeof(SecondLife.Startup))]
+
+namespace SecondLife
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<UserManager<ApplicationUser>>(CreateUserManager);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Home/Login"),
+                ExpireTimeSpan = System.TimeSpan.FromDays(7)
+            });
+        }
+
+        public static UserManager<ApplicationUser> CreateUserManager(IdentityFactoryOptions<UserManager<ApplicationUser>> options, IOwinContext context)
+        {
+            var userManager = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            return userManager;
+        }
+    }
+}
